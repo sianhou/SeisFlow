111adba27c3e32a6dc22a7517936a3b89d5e4bfb
Croco Sian <sianhou1987@gmail.com>
2026-03-09 21:12:45 +0800

    temp save

--- core/segy_dataset.py
+++ core/segy_dataset.py
@@ -37,6 +37,32 @@ class MinMaxToMinus1Plus1:
             return x
 
 
+class PerChannelMinMaxToMinus1Plus1:
+    def __init__(self, eps=1e-12):
+        self.eps = eps
+
+    def __call__(self, x):
+
+        # [C,H,W]
+        if x.ndim == 3:
+            mn = x.amin(dim=(1, 2), keepdim=True)
+            mx = x.amax(dim=(1, 2), keepdim=True)
+
+            x = 2 * (x - mn) / (mx - mn + self.eps) - 1
+            return x
+
+        # [B,C,H,W]
+        elif x.ndim == 4:
+            mn = x.amin(dim=(2, 3), keepdim=True)
+            mx = x.amax(dim=(2, 3), keepdim=True)
+
+            x = 2 * (x - mn) / (mx - mn + self.eps) - 1
+            return x
+
+        else:
+            raise ValueError("Input tensor must be [C,H,W] or [B,C,H,W]")
+
+
 class ClipFirstChannel:
 
     def __init__(self, vmin=None, vmax=None, inplace=False):
