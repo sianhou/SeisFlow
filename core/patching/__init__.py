8e99a5e21fa4a9e9d0c626c529c0be9a04095c84
housian <sianhou1987@gmail.com>
2026-03-12 16:57:32 +0800

    patch_ops:NumpyPatchProcessor、TensorPatchProcessor

--- core/patching/__init__.py
+++ core/patching/__init__.py
@@ -1,9 +1,17 @@
 from .patch_ops import (
+    NumpyPatchProcessor,
+    TensorPatchProcessor,
     extract_overlapping_patches_2d,
+    extract_overlapping_patches_2d_tensor,
     reconstruct_from_overlapping_patches_2d,
+    reconstruct_from_overlapping_patches_2d_tensor,
 )
 
 __all__ = [
+    "NumpyPatchProcessor",
+    "TensorPatchProcessor",
     "extract_overlapping_patches_2d",
+    "extract_overlapping_patches_2d_tensor",
     "reconstruct_from_overlapping_patches_2d",
+    "reconstruct_from_overlapping_patches_2d_tensor",
 ]
