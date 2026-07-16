dc21006fd9dceaee2f4b308111a4b3666aa79e54
housian <sianhou1987@gmail.com>
2026-07-16 17:18:24 +0800

    temp save

---
+++ scripts/hwgpu/build_shot_dataset64.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -u
+
+PYTHON_PATH="/hwdata/24ydz3d/deeplearning/environments/py313/bin/"
+SF_PATH="/hwdata/24ydz3d/deeplearning/SeisFlow-main"
+
+${PYTHON_PATH}/python3.13 ${SF_PATH}/build_shot_dataset.py \
+	--segy ${SF_PATH}/ma2+GathAP.sgy \
+	--patch_size 256 \
+	--overlap_size 64 \
+	--output_dir ${SF_PATH}/shot_dataset256 \
+	--valid 0.0 \
+	--clip -2 2 \
+	--normalize
